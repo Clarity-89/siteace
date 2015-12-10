9eb24b8038b6b5f66f497089671316facf9678a4
Clarity-89 <homes89@ukr.net>
2015-12-10 19:34:42 +0200

    Sort pages by number of upvotes

--- siteace.js
+++ siteace.js
@@ -12,7 +12,7 @@ if (Meteor.isClient) {
     // helper function that returns all available websites
     Template.website_list.helpers({
         websites: function () {
-            return Websites.find({});
+            return Websites.find({}, {sort: {upvotes: -1}});
         }
     });
 
